874e49a25dc62ef2cc1528719572bf9808b26b8c
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-09-03 16:53:55 +0200

    a quick comment before a complete rewrite

--- src/contact-list/contact-list.controller.js
+++ src/contact-list/contact-list.controller.js
@@ -1,25 +1,41 @@
+import * as ContactListItemEvents from '../contact-list-item/contact-list-item.events';
+
 export default class ContactListController {
     constructor($scope, contactGroupAggregateService, contactGroupOrderService) {
+        // Setting fields.
         this.$scope = $scope;
         this.aggregateService = contactGroupAggregateService;
         this.orderService = contactGroupOrderService;
-
+        /**
+         * The contact groups.
+         * 
+         * @type {Array.<ContactGroup>}
+         */
         this.contactGroups = [];
 
-        this.selectedContact = undefined;
+        /**
+         * The selected contact.
+         * 
+         * @type {ContactPresenter|null}
+         */
+        this.selectedContact = null;
 
         this.$scope.$on('searchError', () => {
             alert('searchError!');
         });
-
-        this.$scope.$on('selectContact', (event, payload) => {
+        
+        // Subscribing event handler to the toggle selected to contact event.
+        this.$scope.$on(ContactListItemEvents.CONTACT_LIST_ITEM_TOGGLE_SELECTED_CONTACT, (event, payload) => {
+            // Setting selected contact.
             this.selectedContact = payload.selectedContact;
-
+            // Looping through the contact presenters.
             this.contactPresenters.forEach((contact) => {
-                contact.isActive = payload.selectedContact.id === contact.id;
+                // If we have a selected contact and the ID is the same as the current one's ID, we mark the item as selected.
+                contact.isActive = null !== payload.selectedContact && payload.selectedContact.id === contact.id;
             });
         });
 
+        // Creating contact groups.
         this.$scope.$watch(() => this.contactPresenters, (contacts) => {
             this.groupContacts(contacts);
         });
