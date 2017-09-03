e0946839309f1ef78b77f52ee177451d3dfb730c
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-09-03 17:10:44 +0200

    some routing related changes

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,35 +1,34 @@
-import contactListResolver from './contact-list/contact-list.route-resolver';
 import existingContactResolver from './contact-editor/contact-editor.existing-contact-route-resolver';
 import newContactResolver from './contact-editor/contact-editor.new-contact-route-resolver';
+import groupPresentersResolver from './contact-editor/contact-editor.group-presenters-route-resolver';
 
 export default function appRouting($urlRouterProvider, $stateProvider) {
     $stateProvider
         .state('contacts', {
             component: 'contactList',
-            url: '/',
-            resolve: {
-                contactPresenters: ['contactService', contactListResolver]
-            }
+            url: '/'
         })
         .state('contact', {
             component: 'contactDetails',
             url: '/contacts/{id}',
             resolve: {
-                contact: ['$stateParams', 'contactService', existingContactResolver]
+                contact: ['$state', '$stateParams', 'contactService', existingContactResolver]
             }
         })
         .state('edit', {
             component: 'contactEditor',
             url: '/contacts/{id}/edit',
             resolve: {
-                contact: ['$stateParams', 'contactService', existingContactResolver]
+                contact: ['$state', '$stateParams', 'contactService', existingContactResolver],
+                groupPresenters: ['$q', groupPresentersResolver]
             }
         })
         .state('new', {
             component: 'contactEditor',
             url: '/contacts/create',
             resolve: {
-                contact: ['$q', newContactResolver]
+                contact: ['$q', newContactResolver],
+                groupPresenters: ['$q', groupPresentersResolver]
             }
         });
 
