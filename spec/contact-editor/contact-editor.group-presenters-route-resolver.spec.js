498d3cbf80c712b410485fdf6fdb3ec8cb592b45
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-09-03 11:09:33 +0200

    updated route resolver and unit tests

--- spec/contact-editor/contact-editor.group-presenters-route-resolver.spec.js
+++ spec/contact-editor/contact-editor.group-presenters-route-resolver.spec.js
@@ -1,16 +1,35 @@
+import angular from 'angular';
 import GroupPresenter from '../../src/core/group-presenter.entity';
 import groupPresentersRouteResolver from '../../src/contact-editor/contact-editor.group-presenters-route-resolver';
 
 describe('groupPresentersRouteResolver', () => {
-    it('should map Group instances to GroupPresenter instances', () => {
-        groupPresentersRouteResolver().forEach((item) => {
-            expect(item instanceof GroupPresenter).toBe(true);
+    let _$rootScope_ = undefined;
+    let _$q_ = undefined;
+
+    beforeEach(() => {
+        angular.injector(['ng']).invoke(($rootScope, $q) => {
+            _$rootScope_ = $rootScope;
+            _$q_ = $q;
         });
     });
 
-    it('should create deselected items', () => {
-        groupPresentersRouteResolver().forEach((item) => {
-            expect(item.isSelected).toBe(false);
+    it('should map Group instances to GroupPresenter instances', () => {
+        groupPresentersRouteResolver(_$q_).then((groupPresenters) => {
+            groupPresenters.forEach((item) => {
+                expect(item instanceof GroupPresenter).toBe(true);
+            });
         });
+
+        _$rootScope_.$digest();
+    });
+
+    it('should create deselected items', () => {
+        groupPresentersRouteResolver(_$q_).then((groupPresenters) => {
+            groupPresenters.forEach((item) => {
+                expect(item.isSelected).toBe(false);
+            });
+        });
+
+        _$rootScope_.$digest();
     });
 });
--- src/contact-editor/contact-editor.group-presenters-route-resolver.js
+++ src/contact-editor/contact-editor.group-presenters-route-resolver.js
@@ -5,15 +5,20 @@ import GroupPresenter from '../core/group-presenter.entity';
  * Maps the Group instances to GroupPresenter instances.
  * 
  * @export
+ * @param {Object} $q Angular $q service.
  * @returns {Array.<GroupPresenter>}
  */
-export default function groupPresentersRouteResolver() {
+export default function groupPresentersRouteResolver($q) {
     // The array of groups.
     const GROUPS = [
         new Group(1, 'Family'),
         new Group(2, 'Friends'),
         new Group(3, 'Work')
     ];
-    // Mapping groups to GroupPresenter instances.
-    return GROUPS.map((group) => new GroupPresenter(group));
+    // Creating deferred object.
+    const DEFERRED = $q.defer();
+    // Resolving deferred object with GroupPresenter instances.
+    DEFERRED.resolve(GROUPS.map((group) => new GroupPresenter(group)));
+    // Returning the promise.
+    return DEFERRED.promise;
 }
